e6207dece478a328e6daabbc018bc2fbaf4a5dd3
Yurii Ohlii <jukbq77@gmail.com>
2021-07-29 11:45:09 +0300

    29072021
    Створеня списку фільмів

--- js/main.js
+++ js/main.js
@@ -1,31 +1,44 @@
 $('.serchBtn').click(function(e) {
-    e.preventDefault();
+
     let serch = $('.serchArr').val()
 
-    console.log(serch);
+
 
     let url = 'http://www.omdbapi.com/?i=tt3896198&apikey=bc91fe78&s=*' +
         serch;
 
     async function get() {
+        e.preventDefault();
         try {
             const response = await fetch(url);
             const data = await response.json();
-            let listCreat = '<ul>'
-            let movirList = $.each(data.Search, function(index, value) {
-                listCreat += '<li>';
-                listCreat += '<img src="' + value.Poster + '"> ';
-                listCreat += '' + value.Title + ' ';
-                listCreat += '' + value.Type + ' ';
-                listCreat += '' + value.Year + ' ';
+            if (data.Response == "False") {
+                $('#movieList').empty();
+                let error = '<h1>!!!NO ONE FOUND FOR YOUR INQUIRY!!!</h1>'
+                $('#error').html(error);
+            } else {
+                $('#error').empty();
+                let listCreat = '<ul>'
+                $.each(data.Search, function(index, value) {
+                    listCreat += '<li>';
+                    if (value.Poster == 'N/A') {
+                        listCreat += '<img src="/img/noposter.png"> ';
+                    } else {
+                        listCreat += '<img src="' + value.Poster + '"> ';
+                    }
+                    listCreat += '<div class="movie_titel">' + value.Title + '</div> ';
+                    listCreat += '<p class="movie_type">' + value.Type + '</p> ';
+                    listCreat += '<p class="movie_year">' + value.Year + '</p> ';
+                    listCreat += ' <button class="btn supBtn btn-success moreDetail" type="submit">Nore details</button> ';
+                    listCreat += '</li>';
+                });
+                listCreat += '</ul>';
+                $('#movieList').html(listCreat);
+            }
 
-            });
-            listCreat += '</ul>';
-            $('#movieList').html(listCreat);
-
-            return console.log(data);
         } catch (err) {
-            return console.log(err);
+
+            return console.log(error);
         }
 
 
